a60d1c11d20cdd88d7924d40c666b35308d533a8
Sujan Gurung <sujan.gunner@gmail.com>
2022-05-26 11:21:58 +1200

    sql changes

--- jobs/app.py
+++ jobs/app.py
@@ -1,9 +1,29 @@
-from flask import Flask, render_template
+from multiprocessing import connection
+import sqlite3
 
+from flask import Flask, render_template, g
+
+PATH = "db/jobs.sqlite"
 
 
 app = Flask(__name__)
 
+
+
+def open_connection():
+    connection = getattr(g, '_connection', None)
+
+    if(connection is None):
+        connection = sqlite3.connect(PATH)
+
+    connection.row_factory = sqlite3.Row
+    return connection
+
+
+def execute_sql(sql, values = (), commit = False, single = False):
+    connection = open_connection()
+    cursor = connection.execute(sql, values)
+    return cursor
 
 @app.route('/')
 @app.route('/jobs')

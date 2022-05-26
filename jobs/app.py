f8b962ccf037b9639b17e2f5c612c5c0c07b8795
Sujan Gurung <sujan.gurung@kiwibank.co.nz>
2022-05-26 13:49:23 +1200

    fix

--- jobs/app.py
+++ jobs/app.py
@@ -1,43 +1,33 @@
-from multiprocessing import connection
 import sqlite3
-
 from flask import Flask, render_template, g
 
 PATH = 'db/jobs.sqlite'
 
-
 app = Flask(__name__)
-
-
 
 def open_connection():
     connection = getattr(g, '_connection', None)
-
     if connection == None:
-        connection = sqlite3.connect(PATH)
-
+        connection = g._connection = sqlite3.connect(PATH)
     connection.row_factory = sqlite3.Row
     return connection
 
-
-@app.teardown_appcontext
-def close_connection(exception):
-    connection = getattr(g, '_connection', None)
-    
-    if connection is not None:
-        connection.Close()
-
-def execute_sql(sql, values = (), commit = False, single = False):
+def execute_sql(sql, values=(), commit=False, single=False):
     connection = open_connection()
     cursor = connection.execute(sql, values)
-    
     if commit == True:
         results = connection.commit()
     else:
         results = cursor.fetchone() if single else cursor.fetchall()
 
     cursor.close()
-    return cursor
+    return results
+
+@app.teardown_appcontext
+def close_connection(exception):
+    connection = getattr(g, '_connection', None)
+    if connection is not None:
+        connection.close()
 
 @app.route('/')
 @app.route('/jobs')

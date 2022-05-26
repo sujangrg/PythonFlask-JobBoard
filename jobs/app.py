157b6ba636b408c5f3677168e145cb54a4f6da65
Sujan Gurung <sujan.gurung@kiwibank.co.nz>
2022-05-26 13:47:37 +1200

    fix mod 3

--- jobs/app.py
+++ jobs/app.py
@@ -3,7 +3,7 @@ import sqlite3
 
 from flask import Flask, render_template, g
 
-PATH = "db/jobs.sqlite"
+PATH = 'db/jobs.sqlite'
 
 
 app = Flask(__name__)
@@ -13,7 +13,7 @@ app = Flask(__name__)
 def open_connection():
     connection = getattr(g, '_connection', None)
 
-    if(connection is None):
+    if connection == None:
         connection = sqlite3.connect(PATH)
 
     connection.row_factory = sqlite3.Row
@@ -24,14 +24,14 @@ def open_connection():
 def close_connection(exception):
     connection = getattr(g, '_connection', None)
     
-    if(connection != None):
+    if connection is not None:
         connection.Close()
 
 def execute_sql(sql, values = (), commit = False, single = False):
     connection = open_connection()
     cursor = connection.execute(sql, values)
     
-    if(commit == True):
+    if commit == True:
         results = connection.commit()
     else:
         results = cursor.fetchone() if single else cursor.fetchall()
